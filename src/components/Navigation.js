2746d3ef26cabedc953870f4fbfd8bf98f612a2d
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2022-09-19 22:28:01 +0700

    All Done

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import { AiFillPlusCircle } from 'react-icons/ai';
-import { RiInboxArchiveFill } from 'react-icons/ri';
+
+import { FaArchive } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import '../styles/styles.css';
 export default function Navigation() {
   return (
-    <>
-      <div className="divide-x">Navigation</div>
+    <nav className="p-4">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <Link to="/" className="flex items-center">
+          <h1 className="self-center text-5xl font-semibold whitespace-nowrap ">
+            Personal Notes
+          </h1>
+        </Link>
 
-      <AiFillPlusCircle size={40} />
-      <Link to="/notes/archived">
-        <RiInboxArchiveFill size={40} />
-      </Link>
-    </>
+        <div className="w-full md:block md:w-auto">
+          <ul className="flex mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent ">
+            <li>
+              <Link to="/notes/new" className="block py-2 ">
+                <AiFillPlusCircle size={40} />
+              </Link>
+            </li>
+            <li>
+              <Link to="/notes/archived" className="block py-2 pr-4">
+                <FaArchive size={38} />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
